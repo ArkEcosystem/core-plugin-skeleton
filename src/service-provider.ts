a898ae107420fc3093f1adff943e525029bd4d12
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-15 12:37:01 +0300

    Update and rename plugin.ts to service-provider.ts

---
+++ src/service-provider.ts
@@ -0,0 +1,15 @@
+import { Providers } from "@arkecosystem/core-kernel";
+
+export class ServiceProvider extends Providers.ServiceProvider {
+    public async register(): Promise<void> {
+        //
+    }
+    
+    public async boot(): Promise<void> {
+        //
+    }
+    
+    public async dispose(): Promise<void> {
+        //
+    }
+}
